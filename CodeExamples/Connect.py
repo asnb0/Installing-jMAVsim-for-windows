0c995059f1a61624d8f9e92a8d08b755e8e78403
asnb <asnb1212@gmail.com>
2025-10-20 20:32:13 +0300

    Update Connect.py

--- CodeExamples/Connect.py
+++ CodeExamples/Connect.py
@@ -5,6 +5,6 @@ from mavsdk import System
 async def main():
     drone = System()
     print("Connecting to drone...")
-    await drone.connect(system_address="udp://:14540") // this is the default port
+    await drone.connect(system_address="udp://:14540") # this is the default port
 
 asyncio.run(main())
